c84c4553e434b27f6db3d678aec5b67b41edf966
Venfi <venfioranai@gmail.com>
2025-06-15 13:35:44 +0300

    Model dump properly aliases

--- src/controllers/utils/base_api_model.py
+++ src/controllers/utils/base_api_model.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from pydantic import BaseModel
 from pydantic.alias_generators import to_camel
 
@@ -6,3 +8,6 @@ class BaseApiModel(BaseModel):
     class Config:
         alias_generator = to_camel
         populate_by_name = True
+
+    def model_dump(self, *args, **kwargs) -> dict[str, Any]:
+        return super().model_dump(*args, by_alias=True, **kwargs)
