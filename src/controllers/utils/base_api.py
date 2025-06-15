8a82e77808aea205df33c22772b09990da84ec9d
Venfi <venfioranai@gmail.com>
2025-06-15 11:42:04 +0300

    Added a base api to handle error managemetn

---
+++ src/controllers/utils/base_api.py
@@ -0,0 +1,17 @@
+from flask_restful import Resource, abort
+
+from src.errors.base import BaseError
+
+
+def catch_custom_errors(f: callable):
+    def wrapper(*args, **kwargs):
+        try:
+            f(*args, **kwargs)
+        except BaseError as e:
+            abort(e.status_code, message=str(e))
+
+    return wrapper
+
+
+class BaseApi(Resource):
+    decorators = [catch_custom_errors]
