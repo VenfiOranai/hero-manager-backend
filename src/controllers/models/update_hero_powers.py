bc07f19680ce386b3ae0c64aa384b85dfc4d6f59
Venfi <venfioranai@gmail.com>
2025-06-15 13:14:28 +0300

    Fixed indents

--- src/controllers/models/update_hero_powers.py
+++ src/controllers/models/update_hero_powers.py
@@ -4,9 +4,9 @@ from src.controllers.utils.base_api_model import BaseApiModel
 
 
 class UpdateHeroPowers(BaseApiModel):
-   model_config = ConfigDict(from_attributes=True)
+    model_config = ConfigDict(from_attributes=True)
 
-   hero_id: int
-   powers: list[str] | None = None
+    hero_id: int
+    powers: list[str] | None = None
 
 
