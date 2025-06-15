82debd47787a4559fc7e6aa6e1c0426d4e8e09f6
Venfi <venfioranai@gmail.com>
2025-06-15 12:29:57 +0300

    Implemented get by filters route

--- src/controllers/endpoints/hero_api/heroes.py
+++ src/controllers/endpoints/hero_api/heroes.py
@@ -1,9 +1,23 @@
+from flask import request
+
+from src.controllers.endpoints.hero_api.output_schemas import GetHeroOutputSchema
+from src.controllers.models.hero_filters import HeroFilters
 from src.controllers.utils.base_api import BaseApi
+from src.database.database import session
+from src.services.hero_service import HeroService
 
 
 class HeroesApi(BaseApi):
     def get(self):
-        pass
+        heroes = HeroService(session).get_heroes_by_filters(HeroFilters(**request.args))
+        return [GetHeroOutputSchema(
+            id=hero.id,
+            name=hero.name,
+            suit_color=hero.suit_color,
+            has_cape=hero.has_cape,
+            last_mission=hero.last_mission,
+            is_retired=hero.is_retired,
+        ) for hero in heroes]
 
     def post(self):
         pass
