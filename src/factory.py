2d36800043c1d7aa874a2910b3ab77c89e353627
Venfi <venfioranai@gmail.com>
2025-06-15 11:12:32 +0300

    Initial create app

---
+++ src/factory.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+from src.config import Config
+
+
+def create_app(config: Config) -> Flask:
+    app = Flask(__name__)
+
+    app.config.from_object(config)
+
+    return app
