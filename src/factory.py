f60662c6cfd38ffd86ddede7c52e2a538697596c
Venfi <venfioranai@gmail.com>
2025-06-15 11:15:45 +0300

    Added default to config

--- src/factory.py
+++ src/factory.py
@@ -3,7 +3,7 @@ from flask import Flask
 from src.config import Config
 
 
-def create_app(config: Config) -> Flask:
+def create_app(config: Config = Config) -> Flask:
     app = Flask(__name__)
 
     app.config.from_object(config)
