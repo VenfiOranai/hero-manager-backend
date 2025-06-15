c29e11083c66f84a5cd22b3161c5c2f0dd51edd4
Venfi <venfioranai@gmail.com>
2025-06-15 12:29:20 +0300

    Improved naming

--- src/controllers/models/hero_filters.py
+++ src/controllers/models/hero_filters.py
@@ -4,4 +4,4 @@ from pydantic import BaseModel
 class HeroFilters(BaseModel):
     suit_color: str | None = None
     has_cape: bool | None = None
-    name_contains: str | None = None
+    name: str | None = None
