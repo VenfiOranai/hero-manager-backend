57cde644037b0daa3ec9bf557172b2bc6255363a
idokorkin1 <idokorkin1@gmail.com>
2025-06-15 13:25:55 +0300

    socket io

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from flask_socketio import SocketIO
 from src.factory import create_app
+from src.socketio import socketio_app
 
 if __name__ == '__main__':
     app = create_app()
-    socketio_app = SocketIO(cors_allowed_origins="*")
     socketio_app.init_app(app)
-    socketio_app.run(app, host='0.0.0.0', port=8080, allow_unsafe_werkzeug=True)
+    socketio_app.run(app, debug=True, host='0.0.0.0', port=8080, allow_unsafe_werkzeug=True)
